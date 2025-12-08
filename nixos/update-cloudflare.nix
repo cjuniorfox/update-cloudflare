5d1b70f32526ae5e5f7e8c4a0c5d13fcab604dfe
Junior_FOX <cjuniorfox@gmail.com>
2025-12-08 14:00:53 -0300

    fix: update hash for GitHub source in update-cloudflare package

--- nixos/update-cloudflare.nix
+++ nixos/update-cloudflare.nix
@@ -20,7 +20,7 @@ let
       owner = "cjuniorfox";
       repo = "update-cloudflare";
       rev = "v${version}";
-      sha256 = lib.fakeHash;
+      hash = " sha256-Lq9oVvH8JfgMYudQlT2zdcMAoz1Hcv7gYeAPVu71z/k";
     };
 
     format = "pyproject";
