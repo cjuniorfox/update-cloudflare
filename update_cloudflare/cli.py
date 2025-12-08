76d9fd9d43dcae376dfced78589a7bdd3ea2e58b
Junior_FOX <cjuniorfox@gmail.com>
2025-12-08 11:27:46 -0300

    refactor: change positional arguments to optional for better usability in CLI

--- update_cloudflare/cli.py
+++ update_cloudflare/cli.py
@@ -4,13 +4,13 @@ import argparse
 
 def cli():
     parser = argparse.ArgumentParser(description="Update Cloudflare DNS with current IP.")
-    parser.add_argument("ifname", type=str, default="eth0", help="Network interface name to get the IP from (default: eth0)")
     parser.add_argument("record_name", type=str, default="example.com", help="DNS record name to update (default: example.com)")
-    parser.add_argument("zone_id", type=str, default="", help="Cloudflare zone ID (default: empty)")
-    parser.add_argument("dns_record_id", type=str, default="", help="DNS record ID to update (default: empty)")
-    parser.add_argument("bearer_token", type=str, default="", help="Cloudflare API bearer token (default: empty)")
+    parser.add_argument("--ifname", "-i", type=str, default="eth0", help="Network interface name to get the IP from (default: eth0)")
+    parser.add_argument("--zone_id", "-z", type=str, default="", help="Cloudflare zone ID (default: empty)")
+    parser.add_argument("--dns_record_id", "-d", type=str, default="", help="DNS record ID to update (default: empty)")
+    parser.add_argument("--bearer_token", "-t", type=str, default="", help="Cloudflare API bearer token (default: empty)")
     parser.add_argument("--comment", "-c", type=str, default="", help="Add a comment to the DNS record update (default: empty)")
-    parser.add_argument("--db-path", "-d", type=str, default=None, required=False, help="Path to the database file (default: empty)")
+    parser.add_argument("--db-path", "-D", type=str, default=None, required=False, help="Path to the database file (default: empty)")
     parser.add_argument("--log-level", "-l", type=str, default="INFO", help="Set the logging level (default: INFO)")
     parser.add_argument("--version", "-v", action="version", version=f"update-cloudflare {update_cloudflare.get_version()}", help="Show the version and exit")
     args = parser.parse_args()
