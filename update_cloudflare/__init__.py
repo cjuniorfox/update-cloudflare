aa698853c540f84f7b3fe9fed178896477502062
Junior_FOX <cjuniorfox@gmail.com>
2025-12-08 14:15:12 -0300

    only nixos script on this branch

--- update_cloudflare/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Update Cloudflare DNS records with the current IP address."""
-from update_cloudflare.main import main
-from update_cloudflare.cli import cli
-
-__all__ = ["main", "cli"]
-__version__ = "0.1.0"
-
-def get_version():
-    return __version__
--- update_cloudflare/__main__.py
+++
@@ -1,4 +0,0 @@
-from update_cloudflare.cli import cli
-
-if __name__ == "__main__":
-    cli()
